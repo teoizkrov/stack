a7385bacf11bc6a305e04d2818c3d4b253b9e623
Teo <vinczetti@gmail.com>
2019-11-28 13:48:15 -0500

    rstack init

---
+++ rstack.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <hstack.h>
+
+
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+}
+
+struct Stack
+{
+
+    struct Node *head;
+    int count;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main( int argc, char *argv[] )
+{
+
+
+    return 0;
+}
